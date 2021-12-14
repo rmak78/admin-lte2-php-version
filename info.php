a5083171f933673ac0c31c033eddbdb9c6c729d7
Rana Mansoor Akbar Khan <rmak@sutlej.net>
2021-12-15 01:55:15 +0500

    Update info.php

--- info.php
+++ info.php
@@ -1,17 +1,3 @@
 <?php
-$sender = 'store-manager@brownbag.pk';
-$recipient = 'mansoor@brownabag.pk';
-
-$subject = "php mail test";
-$message = "php test message";
-$headers = 'From:' . $sender;
-
-if (mail($recipient, $subject, $message, $headers))
-{
-    echo "Message accepted";
-}
-else
-{
-    echo "Error: Message not accepted";
-}
+phpinfo();
 ?>
